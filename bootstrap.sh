8dac0b3f429ec84732ecd8af54bcec32938d854d
Stephen Finucane <stephenfinucane@hotmail.com>
2015-10-08 02:46:06 +0100

    Add 'vim' folder
    This will prevent us downloading the vim plugins before we can use it.
    
    Signed-off-by: Stephen Finucane <stephenfinucane@hotmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # create symlinks from the home directory to dotfiles in ~/dotfiles
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-dotfiles="vimrc inputrc bash"
+dotfiles="vimrc vim inputrc bashrc bash"
 
 # symlink dotfiles to the the home dir, each prefixed by a dot (.)
 for file in $dotfiles; do
