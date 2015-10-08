a900b0a17edc8dd2fffe55a71b7137db1b476d12
Stephen <stephenfinucane@hotmail.com>
2015-10-08 17:15:42 +0100

    bootstrap: Add prompt to reload environment
    While we're at, rename to 'dotstrap' as well.
    
    Signed-off-by: Stephen <stephenfinucane@hotmail.com>

--- dotstrap.sh
+++ dotstrap.sh
@@ -10,3 +10,10 @@ for file in $dotfiles; do
     rm -rf ~/.$file
     ln -s $dir/$file ~/.$file
 done
+
+read -p "Reload environment now? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    exec bash
+fi
+
