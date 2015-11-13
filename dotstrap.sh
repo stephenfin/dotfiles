5a93a55ba5ea0a4c470601b123fcfbc832be3d72
Stephen <stephenfinucane@hotmail.com>
2015-11-13 17:34:12 +0000

    dotstrap: Confirm overwrite
    It's not very user-friendly to overwrite files haphazardly. Confirm
    that the user wishes to remove a file before actually doing so.
    
    Signed-off-by: Stephen <stephenfinucane@hotmail.com>

--- dotstrap.sh
+++ dotstrap.sh
@@ -8,13 +8,33 @@ dotfiles="vimrc vim inputrc bashrc bash next_review"
 # download submodules
 git submodule init
 
+function cdiff() {
+    diff -u $@ | sed "s/^-/\x1b[31m-/;s/^+/\x1b[32m+/;s/^@/\x1b[34m@/;s/$/\x1b[0m/"
+}
+
 # symlink dotfiles to the the home dir, each prefixed by a dot (.)
 for file in $dotfiles; do
-    rm -rf ~/.$file
-    ln -s $dir/$file ~/.$file
+    src="$dir/$file"
+    dst="$HOME/.$file"
+
+    if [ -e "$dst" ]; then
+        echo "WARNING! File already exists. Here's a comparison:"
+        echo
+        cdiff "$dst" "$src"
+        read -p "Do you wish to overwrite file? ([y]es/[n]o) " -n 1 -r
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            echo "Skipping this file"
+            continue
+        fi
+    fi
+
+    rm -rf "$dst"
+    ln -s "$src" "$dst"
+    echo "Created $dst"
 done
 
-read -p "Reload environment now? " -n 1 -r
+read -p "Reload environment now? [y]es/[n]o " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     exec bash
